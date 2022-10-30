f28187a8ffc643e5cc04a77ac460fed49bd8b583
victor lopez <113215844+viclop1597@users.noreply.github.com>
2022-10-30 14:34:34 -0600

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,44 @@
+package com.generation;
+
+//Se importan las paqueterias
+import java.util.Scanner;
+import java.util.*;
+
+public class Codigo4 {
+	
+	//Se agrega el main
+	public static void main(String[] args) {
+    Scanner s = new Scanner();
+    
+    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+    String j1 = s.nextLine();
+    
+    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+    Scanner s2 = new Scanner();
+    String j2 = s.nextLine();
+    
+	    if (j1 == j2)) {
+	      System.out.println("Empate");
+	    } else {
+	      int g = 2;
+	      switch(j1) {
+	      
+	        case "piedra":
+	          if (j2 == "tijeras") {
+	            g = 1;
+	          }break //Se agrega el break para no pasar al siguiente
+	
+	        case "papel":
+	          if (j2 == "piedra") {
+	            g = 1;
+	          }break //Se agrega el break para no pasar al siguiente
+	          
+	        case "tijera":
+	          if (j2.equals("papel")) {
+	            g = 1;
+	          }break;
+	        default:
+	      } System.out.println("Gana el jugador " + g);
+	      }
+	    }
+	}
